60b0127916e3f33f290cf99b4681d9c441cb97e9
kuresti <55415865+kuresti@users.noreply.github.com>
2026-01-17 20:07:15 -0700

    Added Pizza

--- ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f451c937fac221b32ad6ce9c2ce6f784bd950492")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcc6bace20922a1a39464fd0cb02fa202ffeaa49")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
