7c9a32b79cecbb66b0d3455ef92b5fc998801b2a
kuresti <55415865+kuresti@users.noreply.github.com>
2026-01-23 10:24:51 -0700

    Uresti Movies initial commit

---
+++ UrestiMovieMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UrestiMovieMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
